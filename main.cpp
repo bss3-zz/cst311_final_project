17c6112fe68289d2f57f98dc26dcde94c979cfc1
Joao Marcos Costa Salles <jaosalless@gmail.com>
2014-11-28 21:17:42 -0800

    second part of project- accepting adj table

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <climits>
 #include <queue>
 
+
+void update(edge.weight,)
+
 //for debug purposes
 #include <typeinfo>
 
@@ -87,8 +90,34 @@ void linkEdges(Edge * TempEdge, list<Node> nodes){//Count nodes from 0, this fuc
 };
 
 
+
+String nameUpadate(String name){ //fuction to give a unique name to each node
+    int size = name.size() - 1;
+    if(name[size] == 'z'){
+        zCase(name,size);
+    }else{
+        name[size]++;
+    }
+    return name;
+}
+
+void zCase(String name, int size){// Z treatment
+    if(size == 0){
+        name[size] = 'a';
+        name =  name + "a";
+    }else{
+        name[size] = 'a';
+        if(name[size-1] == 'z'){
+            zCase(name,size-1);
+        }else{
+            name[size-1]++;        }
+    }
+}
+
+
 int main(){
     FILE *input, *output;
+    int l,row
     
     //Redirecting the standard input from to the input file
     input = freopen("input.in","r",stdin);
@@ -112,39 +141,44 @@ int main(){
     
     printf("Loading nodes...\n");
     //Reading the node description from file
+
+    string name;
     for (int i = 0; i < n_nodes; i++) {
-        int temp_id;
-        string temp_name;
-        cin >> temp_id;
-        cin >> temp_name;
-        
-        //Init distances
-        if(temp_id == s_node){ //0 of distance if the node is the start node
-            nodes[temp_id].distance = 0;
-        }else{ //Infinite distance if the node is not the start node
-            nodes[temp_id].distance = -1;
+        if(name.size == 0 ){
+            name = 'a';
+            odes[temp_id].id = i;
+            nodes[temp_id].name = 'a';
+            nodes[temp_id].visited = false;
+        }else{
+            if(i == s_node){ //0 of distance if the node is the start node
+                nodes[i].distance = 0;
+            }else{ //Infinite distance if the node is not the start node
+                nodes[temp_id].distance = -1;
+            }
+            nodes[temp_id].id = i;
+            nodes[temp_id].name = nameUpadate(name);
+            nodes[temp_id].visited = false;
         }
         
-        nodes[temp_id].id = temp_id;
-        nodes[temp_id].name = temp_name;
-        nodes[temp_id].visited = false;
     }
     
+    
+    
+    for (l = 0; l < n_nodes; l++) {
+        for (row = 0; row < n_nodes; row++) {
+            if(row != l){
+                cin >> weight;
+                Edge *temp_edge = new Edge(row,l, weight);
+                Edge *temp_edge2 = new Edge(l,row, weight);
+                nodes[row].adjacentList.push_back(*temp_edge);
+                nodes[l].adjacentList.push_back(*temp_edge2);
+            }
+        }
+    }
     //All nodes now has been loaded into the structures
     
     printf("We have %lu nodes to work.\n", nodes.size());
-    printf("Loading edges...\n");
-    
-    for (int i = 0; i < n_edges; i++) {
-        int source, destination, weight;
-        cin >> source;
-        cin >> destination;
-        cin >> weight;
-        cout << source << " " << destination << " " << weight << endl;
-        Edge *temp_edge = new Edge(source,destination,weight);
-        nodes[source].adjacentList.push_back(*temp_edge);
-        nodes[destination].adjacentList.push_back(*temp_edge);
-    }
+    printf("Loading edges...\n")
     
     priority_queue <Node,vector<Node>,NodeCompare> pq; //Creating a priority queue to store the nodes during the computation of the shortest path
     
